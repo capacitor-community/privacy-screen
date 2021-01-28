1e6942cf76980a7053fd962d29032e7366768cad
Robin Genz <mail@robingenz.dev>
2021-01-28 17:05:39 +0100

    build: update rollup.config.js (#15)

--- rollup.config.js
+++ rollup.config.js
@@ -5,17 +5,11 @@ export default {
   output: {
     file: 'dist/plugin.js',
     format: 'iife',
-    name: 'capacitorPlugin', // TODO: change this
+    name: 'capacitorPrivacyScreen',
     globals: {
       '@capacitor/core': 'capacitorExports',
     },
     sourcemap: true,
   },
-  plugins: [
-    nodeResolve({
-      // allowlist of dependencies to bundle in
-      // @see https://github.com/rollup/plugins/tree/master/packages/node-resolve#resolveonly
-      resolveOnly: ['lodash'],
-    }),
-  ],
+  plugins: [nodeResolve()],
 };
