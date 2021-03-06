1f99d68f23f4f078c35fcbbbb4535aaa81dc42c4
Robin Genz <mail@robingenz.dev>
2021-03-06 19:33:18 +0100

    remove deprecated WebPluginConfig

--- src/web.ts
+++ src/web.ts
@@ -3,13 +3,6 @@ import { WebPlugin } from '@capacitor/core';
 import type { PrivacyScreenPlugin } from './definitions';
 
 export class PrivacyScreenWeb extends WebPlugin implements PrivacyScreenPlugin {
-  constructor() {
-    super({
-      name: 'PrivacyScreen',
-      platforms: ['web'],
-    });
-  }
-
   async enable(): Promise<void> {
     throw this.unimplemented('Not implemented on web.');
   }
