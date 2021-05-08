01c553c670d0a9fdff066c73b7548a5f8b0ea328
Robin Genz <mail@robingenz.dev>
2021-05-08 12:10:43 +0200

    refactor: apply Capacitor 3 plugin template

--- ios/PluginTests/PrivacyScreenPluginTests.swift
+++ ios/PluginTests/PrivacyScreenPluginTests.swift
@@ -1,8 +1,7 @@
 import XCTest
 @testable import Plugin
 
-class PrivacyScreenPluginTests: XCTestCase {
-
+class PrivacyScreenTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,18 +16,10 @@ class PrivacyScreenPluginTests: XCTestCase {
         // This is an example of a functional test case for a plugin.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
 
+        let implementation = PrivacyScreen()
         let value = "Hello, World!"
-        let plugin = MyPlugin()
+        let result = implementation.echo(value)
 
-        let call = CAPPluginCall(callbackId: "test", options: [
-            "value": value
-        ], success: { (result, _) in
-            let resultValue = result!.data["value"] as? String
-            XCTAssertEqual(value, resultValue)
-        }, error: { (_) in
-            XCTFail("Error shouldn't have been called")
-        })
-
-        plugin.echo(call!)
+        XCTAssertEqual(value, result)
     }
 }
