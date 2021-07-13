770a42384d89a6155a86ccce5c42a55c28947fb5
a.fatullaev <a.fatullaev@bta.kg>
2021-07-13 23:35:58 +0600

    Blur background

--- ios/Plugin/PrivacyScreenPlugin.swift
+++ ios/Plugin/PrivacyScreenPlugin.swift
@@ -9,7 +9,7 @@ import Capacitor
 public class PrivacyScreenPlugin: CAPPlugin {
     private var isEnabled = true
     private var privacyViewController: UIViewController?
-    
+
     override public func load() {
         NotificationCenter.default.addObserver(self, selector: #selector(self.onAppDidBecomeActive),
                                                name: UIApplication.didBecomeActiveNotification, object: nil)
@@ -20,12 +20,12 @@ public class PrivacyScreenPlugin: CAPPlugin {
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
-    
+
     @objc func enable(_ call: CAPPluginCall) {
         self.isEnabled = true
         call.resolve()
     }
-    
+
     @objc func disable(_ call: CAPPluginCall) {
         self.isEnabled = false
         call.resolve()
@@ -35,9 +35,20 @@ public class PrivacyScreenPlugin: CAPPlugin {
         guard self.isEnabled else {
             return
         }
+
         self.privacyViewController = UIViewController()
-        self.privacyViewController!.view.backgroundColor = UIColor.gray;
+        self.privacyViewController!.view.backgroundColor = .clear;
         self.privacyViewController!.modalPresentationStyle = UIModalPresentationStyle.overFullScreen
+        if !UIAccessibility.isReduceTransparencyEnabled {
+            let blurEffect = UIBlurEffect(style: .dark)
+            let blurEffectView = UIVisualEffectView(effect: blurEffect)
+            blurEffectView.frame = self.privacyViewController!.view.bounds
+            blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            self.privacyViewController!.view.addSubview(blurEffectView) //if you have more UIViews, use an insertSubview API to place it where needed
+        } else {
+            self.privacyViewController!.view.backgroundColor = .gray
+        }
+
         DispatchQueue.main.async {
             self.bridge?.viewController?.present(self.privacyViewController!, animated: false, completion: nil)
         }
