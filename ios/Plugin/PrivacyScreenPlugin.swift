af896289f2d223bc0b4efbe47ad74e8e69ea72b0
a.fatullaev <a.fatullaev@bta.kg>
2021-08-20 00:27:07 +0600

    Added: UIAlertController didDetectRecording didDetectScreenshot

--- ios/Plugin/PrivacyScreenPlugin.swift
+++ ios/Plugin/PrivacyScreenPlugin.swift
@@ -9,12 +9,17 @@ import Capacitor
 public class PrivacyScreenPlugin: CAPPlugin {
     private var isEnabled = true
     private var privacyViewController: UIViewController?
+    private var alert: UIAlertController?
 
     override public func load() {
         NotificationCenter.default.addObserver(self, selector: #selector(self.onAppDidBecomeActive),
                                                name: UIApplication.didBecomeActiveNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.onAppWillResignActive),
                                                name: UIApplication.willResignActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.didDetectRecording),
+                                               name: UIScreen.capturedDidChangeNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.didDetectScreenshot),
+                                               name: UIApplication.userDidTakeScreenshotNotification, object: nil)
     }
 
     deinit {
@@ -57,6 +62,36 @@ public class PrivacyScreenPlugin: CAPPlugin {
     @objc func onAppDidBecomeActive() {
         DispatchQueue.main.async {
             self.privacyViewController?.dismiss(animated: false, completion: nil)
+        }
+    }
+    @objc private func didDetectRecording() {
+        guard self.isEnabled else {
+            return
+        }
+
+        if #available(iOS 11.0, *) {
+            if UIScreen.main.isCaptured {
+                DispatchQueue.main.async {
+                    self.bridge?.viewController?.present(self.privacyViewController!, animated: false, completion: nil)
+                }
+            } else {
+                self.privacyViewController?.dismiss(animated: false, completion: nil)
+            }
+        }
+
+    }
+
+    @objc private func didDetectScreenshot() {
+        guard self.isEnabled else {
+            return
+        }
+
+        self.alert = UIAlertController(title: "Attention",
+                                      message: "Screenshots may contain sensitive data. Do not keep them on your phone!",
+                                      preferredStyle: .alert)
+        self.alert?.addAction(UIAlertAction(title: "OK", style: .default))
+        DispatchQueue.main.async {
+            self.bridge?.viewController?.present(self.alert!, animated: false, completion: nil)
         }
     }
 }
