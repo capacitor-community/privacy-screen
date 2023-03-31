c5472227db72cdea3928ee03d5c77190101206af
Robin Genz <mail@robingenz.dev>
2023-03-31 20:31:54 +0200

    fix(ios): `removeAllListeners` method not found

--- ios/Plugin/PrivacyScreenPlugin.m
+++ ios/Plugin/PrivacyScreenPlugin.m
@@ -6,4 +6,5 @@
 CAP_PLUGIN(PrivacyScreenPlugin, "PrivacyScreen",
            CAP_PLUGIN_METHOD(enable, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(disable, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(removeAllListeners, CAPPluginReturnNone);
 )
