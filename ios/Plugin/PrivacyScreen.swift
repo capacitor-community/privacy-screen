b7ff236a6bb98d08ec851c7d8614e04dc046eec7
jcesarmobile <jcesarmobile@gmail.com>
2024-02-16 18:49:08 +0100

    fix(ios): Present the privacyViewController on the top ViewController

--- ios/Plugin/PrivacyScreen.swift
+++ ios/Plugin/PrivacyScreen.swift
@@ -41,7 +41,15 @@ import UIKit
             return
         }
         DispatchQueue.main.async {
-            self.plugin.bridge?.viewController?.present(self.privacyViewController, animated: false, completion: nil)
+            let window = UIApplication.shared.connectedScenes.filter({$0.activationState == .foregroundActive || $0.activationState == .foregroundInactive}).compactMap({$0 as? UIWindowScene}).first?.windows.filter({$0.isKeyWindow}).first
+            if var rootVC = window?.rootViewController {
+                while let topVC = rootVC.presentedViewController {
+                    rootVC = topVC
+                }
+                rootVC.present(self.privacyViewController, animated: false, completion: nil)
+            } else {
+                self.plugin.bridge?.viewController?.present(self.privacyViewController, animated: false, completion: nil)
+            }
         }
     }
 
