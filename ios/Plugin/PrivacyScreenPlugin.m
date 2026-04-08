b19a7566ffdd0cc7fd620a19170bd666fa748fa1
Tafel <35837839+tafelnl@users.noreply.github.com>
2026-04-08 10:25:53 +0200

    feat: add SPM support (#117)
    * feat: run `cap2spm`
    
    * feat: rectify some files
    
    The `cap2spm` isn't perfect still. So we needed to do three things:
    
    - make the `.gitignore` consistent with other plugins
    - run `npm run fmt`
    - we revert back to Capacitor v7 support in `Package.swift`

--- ios/Plugin/PrivacyScreenPlugin.m
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Capacitor/Capacitor.h>
-
-// Define the plugin using the CAP_PLUGIN Macro, and
-// each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(PrivacyScreenPlugin, "PrivacyScreen",
-           CAP_PLUGIN_METHOD(enable, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(disable, CAPPluginReturnPromise);
-)
--- ios/Tests/PrivacyScreenPluginTests/PrivacyScreenPluginTests.swift
+++ ios/Tests/PrivacyScreenPluginTests/PrivacyScreenPluginTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import Plugin
+@testable import PrivacyScreenPlugin
 
 class PrivacyScreenTests: XCTestCase {
 
